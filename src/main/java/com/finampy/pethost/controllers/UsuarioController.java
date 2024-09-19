fbd327e49a5ffef51db7cd528810a16e002b6d7b
Nailton S. <44982114+nailtonsimoes@users.noreply.github.com>
2024-09-18 23:39:41 -0300

    feat: criação do projeto

---
+++ src/main/java/com/finampy/pethost/controllers/UsuarioController.java
@@ -0,0 +1,19 @@
+package com.finampy.pethost.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UsuarioController {
+
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public String teste() {
+        var nome = "esse é nosso endpoint";
+        return nome;
+    }
+}
---
+++ src/test/java/com/finampy/pethost/PethostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finampy.pethost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PethostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
