a00aa76bb0d90711c0b90200d3a1e87c575b589d
Nailton S. <44982114+nailtonsimoes@users.noreply.github.com>
2024-09-18 23:39:41 -0300

    feat: criação do projeto

---
+++ src/main/java/com/finampy/pethost/PethostApplication.java
@@ -0,0 +1,13 @@
+package com.finampy.pethost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PethostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PethostApplication.class, args);
+		System.out.println("Api Rodando dog do bem");
+	}
+}
