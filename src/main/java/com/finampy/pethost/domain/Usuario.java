f2fe421edd975d90fe18987d91b148ea61d69423
Nailton S. <44982114+nailtonsimoes@users.noreply.github.com>
2024-09-19 01:43:23 -0300

    config do banco de dados

--- src/main/java/com/finampy/pethost/domain/Usuario.java
+++ src/main/java/com/finampy/pethost/domain/Usuario.java
@@ -1,4 +1,19 @@
 package com.finampy.pethost.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_USER")
 public class Usuario {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 }
