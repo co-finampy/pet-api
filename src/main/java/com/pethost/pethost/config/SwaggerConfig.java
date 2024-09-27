3da688ad95de1e62944c064f440248e3dce811c4
Nailton S. <44982114+nailtonsimoes@users.noreply.github.com>
2024-09-27 01:09:52 -0300

    feat/swagger: ajuste de versão do spring para 3.2.10 e refatoração de config de swagger

--- src/main/java/com/pethost/pethost/config/SwaggerConfig.java
+++ src/main/java/com/pethost/pethost/config/SwaggerConfig.java
@@ -1,21 +1,20 @@
 package com.pethost.pethost.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import org.springdoc.core.GroupedOpenApi;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "Todo Web App API", version = "0.1", description = "Aplicação para cadastro de tarefas")
-)
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("com.pethost.pethost.controllers")
-                .pathsToMatch("/v1/**")
-                .build();
-    }
+   @Bean
+    public OpenAPI customOpenAPI() {
+       return new OpenAPI()
+               .info(new Info()
+                       .title("PetHost API")
+                       .description("descrição da api")
+                       .contact(new Contact().name("PETHOST")
+                       ).version("1.0.0"));
+   }
 }
