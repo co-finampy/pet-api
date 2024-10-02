dc431a499bb9f8a439672896890292dafd0cb9a4
Nailton S. <44982114+nailtonsimoes@users.noreply.github.com>
2024-10-02 01:38:56 -0300

    implementação de spring security

--- src/main/java/com/pethost/pethost/repositories/PetRepository.java
+++ src/main/java/com/pethost/pethost/repositories/PetRepository.java
@@ -3,11 +3,10 @@ package com.pethost.pethost.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pethost.pethost.domain.Pet;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
-
     Pet findById(long id);
-
 }
--- src/main/java/com/pethost/pethost/repositories/UsuarioRepository.java
+++ src/main/java/com/pethost/pethost/repositories/UsuarioRepository.java
@@ -2,9 +2,11 @@ package com.pethost.pethost.repositories;
 
 import com.pethost.pethost.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
