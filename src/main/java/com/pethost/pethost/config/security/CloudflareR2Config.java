8dea0395004ade355beadfd765ed3f48357fd6e6
Nailton S. <44982114+nailtonsimoes@users.noreply.github.com>
2024-10-25 00:36:37 -0300

    alteracao de variaveis de ambiente

--- src/main/java/com/pethost/pethost/config/security/CloudflareR2Config.java
+++ src/main/java/com/pethost/pethost/config/security/CloudflareR2Config.java
@@ -8,19 +8,29 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+
 @Configuration
 public class CloudflareR2Config {
 
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretKey}")
+    private String secretAccessKey;
+
+    @Value("${aws.s3Endpoint}")
+    private String endpoint;
+
+    @Value("${aws.region}")
+    private String region;
+
     @Bean
     public AmazonS3 amazonS3() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIAVVZPCLWLGBJLXZGC", // Access Key ID
-                "vGw0gkiIRf8VUmHiZeF2ryErGDw5WLnOZuhIsZUH" // Secret Access Key
-        );
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
 
         return AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
-                        "https://s3.amazonaws.com", "us-east-2")) // Substitua pelo seu endpoint e região
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
                 .withPathStyleAccessEnabled(true)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
