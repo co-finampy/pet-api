97024b5cfde1af5c38cc9aeb3adeae6ad6016abe
Nailton S. <44982114+nailtonsimoes@users.noreply.github.com>
2024-11-01 09:03:18 -0300

    feat: criação de listagem e ajude de criacao de pet

--- src/main/java/com/pethost/pethost/controllers/PetsController.java
+++ src/main/java/com/pethost/pethost/controllers/PetsController.java
@@ -1,7 +1,11 @@
 package com.pethost.pethost.controllers;
 
 import com.pethost.pethost.domain.Pet;
+import com.pethost.pethost.domain.Usuario;
+import com.pethost.pethost.dtos.CriarPetRequestDto;
 import com.pethost.pethost.services.PetService;
+import com.pethost.pethost.services.UsuarioService;
+import com.pethost.pethost.utils.DataUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,11 +29,24 @@ public class PetsController {
     @Autowired
     private PetService petService;
 
+    @Autowired
+    private UsuarioService usuarioService;
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
     // Listar todos os pets
     @GetMapping("/listar")
     @Operation(summary = "Listar pets", description = "Responsável por listar todos os pets")
     public ResponseEntity<List<Pet>> listarPets() {
         List<Pet> pets = petService.findAllPets();
+        return new ResponseEntity<>(pets, HttpStatus.OK);
+    }
+
+    // Listar todos os pets baseado em um Uid de usuario
+    @GetMapping("{uid}/listar")
+    @Operation(summary = "Listar pets por Uid do usuario", description = "Responsável por listar todos os pets baseado em um Uid de usuario")
+    public ResponseEntity<List<Pet>> listarPetsByWonerUid(@PathVariable(value = "uid") String uid) {
+        List<Pet> pets = petService.findAllPetsByUid(uid);
         return new ResponseEntity<>(pets, HttpStatus.OK);
     }
 
@@ -40,10 +61,27 @@ public class PetsController {
 
     // Criar um novo pet
     @PostMapping("/criar")
-    @Operation(summary = "Criar pet", description = "Responsável por criar um novo pet")
-    public ResponseEntity<Pet> criarPet(@RequestBody Pet pet) {
-        Pet createdPet = petService.criarPet(pet);
-        return new ResponseEntity<>(createdPet, HttpStatus.CREATED);
+    @Operation(summary = "Buscar pet por ID", description = "Responsável por buscar um único pet pelo ID")
+    public ResponseEntity<Pet> criarPet(@RequestBody CriarPetRequestDto criarPetRequestDto) {
+        // Converte CriarPetRequestDto para Pet
+        Pet pet = Pet.builder()
+                .tipoPet(criarPetRequestDto.getTipoPet())
+                .nomePet(criarPetRequestDto.getNomePet())
+                .raca(criarPetRequestDto.getRaca())
+                .genero(criarPetRequestDto.getGenero())
+                .tamanho(criarPetRequestDto.getTamanho())
+                .dataNascimento(DataUtils.converterData(criarPetRequestDto.getDataNascimento()))
+                .vacina(criarPetRequestDto.getVacina())
+                .castrado(criarPetRequestDto.getCastrado())
+                .foto(criarPetRequestDto.getFoto())
+                .criadoEm(LocalDateTime.now())
+                .usuario(usuarioService.findByUid(criarPetRequestDto.getUidUsuario())) // Associa o usuário
+                .build();
+
+        // Chama o serviço para salvar o pet
+        petService.criarPet(pet);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(pet);
     }
 
     // Atualizar um pet existente
