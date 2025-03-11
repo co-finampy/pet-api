376fd4b025c4478631ed8a3af373af3feae0140a
ksilvafe <ferreirakevin325@gmail.com>
2025-03-10 22:09:17 -0300

    add cors

--- src/main/java/com/pethost/pethost/config/security/SecurityConfiguration.java
+++ src/main/java/com/pethost/pethost/config/security/SecurityConfiguration.java
@@ -45,8 +45,8 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-        configuration.setAllowedOrigins(List.of("https://pet-api-production.up.railway.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000/**"));
+        configuration.setAllowedOrigins(List.of("https://pet-api-production.up.railway.app/**"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("authorization", "content-type"));
 
