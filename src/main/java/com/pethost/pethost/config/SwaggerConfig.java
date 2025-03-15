12f927d5b0e42d4dfe81afc77612644880ceb3d4
allankdev <allankelven.ak@gmail.com>
2025-03-15 11:16:45 -0300

    ajuste no swagger config para aceitar https

--- src/main/java/com/pethost/pethost/config/SwaggerConfig.java
+++ src/main/java/com/pethost/pethost/config/SwaggerConfig.java
@@ -6,21 +6,24 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-   @Bean
+    @Bean
     public OpenAPI customOpenAPI() {
-       return new OpenAPI()
-               .info(new Info()
-                       .title("PetHost API")
-                       .description("descrição da api")
-                       .contact(new Contact().name("PETHOST")
-                       ).version("1.0.0"))
-               .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-               .components(new Components().addSecuritySchemes("bearerAuth",
-                       new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
-   }
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PetHost API")
+                        .description("descrição da api")
+                        .contact(new Contact().name("PETHOST"))
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                // 🔥 Adicionando a URL correta para evitar erro de Mixed Content
+                .addServersItem(new Server().url("https://pet-api-production.up.railway.app"));
+    }
 }
